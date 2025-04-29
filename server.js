409f279ff53f765700739910b6ed2561bf54d13c
emmeiwhite <emmeiwhite@gmail.com>
2025-04-29 11:40:14 +0530

    express-2025 health route to test infrastructure of our application

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ const app = express()
 
 const PORT = process.env.PORT || 4000
 
+// 1. Routing
+app.get('/health', (req, res) => {
+  console.log('Infrastructure health report being checked')
+  res.send('Welcome to Coders Bank')
+})
+
 app.listen(PORT, () => console.log(`server is listening on port: ${PORT} `))
