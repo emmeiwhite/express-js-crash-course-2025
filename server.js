d14e847883ae2a3e5da55ad8973b0204f677024a
emmeiwhite <emmeiwhite@gmail.com>
2025-04-29 11:51:37 +0530

    express-2025 sending back json response

--- server.js
+++ server.js
@@ -6,8 +6,14 @@ const PORT = process.env.PORT || 4000
 
 // 1. Routing
 app.get('/health', (req, res) => {
+  /* --
   console.log('Infrastructure health report being checked')
-  res.send('Welcome to Coders Bank')
+  res.send('Hey! I am healthy')
+  -- */
+
+  // Sending Back JSON response --- means content-type:application/json
+
+  res.status(400).send({ message: 'One machine is down', success: true })
 })
 
 app.listen(PORT, () => console.log(`server is listening on port: ${PORT} `))
