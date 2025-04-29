a6bf693da57d2f9854792f8def9bdcb5f5c20e42
emmeiwhite <emmeiwhite@gmail.com>
2025-04-29 12:24:20 +0530

    express-2025 sending data from client to server in body

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 4000
 
 // 1. Routing
 app.get('/health', (req, res) => {
+  console.log(req.query)
   /* --
   console.log('Infrastructure health report being checked')
   res.send('Hey! I am healthy')
@@ -14,6 +15,13 @@ app.get('/health', (req, res) => {
   // Sending Back JSON response --- means content-type:application/json
 
   res.status(400).send({ message: 'One machine is down', success: true })
+})
+
+// 2. Sending data from client to server (body object)
+app.post('/api/users', (req, res) => {
+  const body = req.body
+  console.log('body', body)
+  res.json({})
 })
 
 app.listen(PORT, () => console.log(`server is listening on port: ${PORT} `))
