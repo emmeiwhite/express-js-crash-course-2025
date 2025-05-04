95eb61d191649b5213ea591a3ceb9d77bc1df75d
emmeiwhite <emmeiwhite@gmail.com>
2025-05-04 15:10:23 +0530

    express-2025 simple route setting home and about pages

--- app.js
+++ app.js
@@ -13,4 +13,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'about.html'))
+})
+
 app.listen(3000, () => console.log(`Server is listening on port: 3000`))
