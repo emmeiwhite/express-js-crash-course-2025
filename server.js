9f736d7d36d3d27c5d7d15f44e30dd97b1512eb9
emmeiwhite <emmeiwhite@gmail.com>
2025-05-07 15:23:36 +0530

    express-2025 revisit with mind map of steps (practice is the key to any success)

--- server.js
+++ server.js
@@ -1,40 +1,10 @@
+// Revision with server.js
+
+/** Step-1: We create our express server */
+
 import express from 'express'
+const server = express()
 
-const app = express()
+const PORT = process.env.PORT || 3000
 
-/** --- Logger Middleware  --- */
-const reqLogger = (req, res, next) => {
-  console.log(`${req.method} ${req.url} ${new Date().toISOString()}`)
-
-  next()
-}
-
-// 3. We have to enable body parser in Express to parse body data coming from the client
-app.use(express.json())
-
-//
-app.use(reqLogger)
-
-// 1. Routing
-app.get('/health', (req, res) => {
-  console.log(req.query)
-  /* --
-  console.log('Infrastructure health report being checked')
-  res.send('Hey! I am healthy')
-  -- */
-
-  // Sending Back JSON response --- means content-type:application/json
-
-  res.status(400).send({ message: 'One machine is down', success: true })
-})
-
-// 2. Sending data from client to server (body object)
-app.post('/api/users', (req, res) => {
-  const body = req.body
-  console.log('body', body)
-  res.json({})
-})
-
-const PORT = process.env.PORT || 4000
-
-app.listen(PORT, () => console.log(`server is listening on port: ${PORT} `))
+server.listen(PORT, () => console.log(`Listening on port ${PORT} ...`))
