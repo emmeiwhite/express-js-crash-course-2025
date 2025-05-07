72b8f1560786e65ff734d0f2216fa009e628075f
emmeiwhite <emmeiwhite@gmail.com>
2025-05-07 17:13:27 +0530

    express-2025 ejs configured and routes setup

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 import express from 'express'
 const app = express()
 
+/** Step-1: Tell Express about which template engine we are using */
+app.set('view engine', 'ejs')
+console.log(app.get('view engine'))
+
+/** Step-2: Tell Express which folder to look for serving files: by default it is views folder */
+console.log(app.get('views'))
+
+/** Step-3: Setup Routes to serve dynamic sites with ejs */
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
