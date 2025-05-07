3dc0b4ab0da6469b365431474f62094ba48ec253
emmeiwhite <emmeiwhite@gmail.com>
2025-05-07 20:17:04 +0530

    express-2025 successfully configured router in our app

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,23 @@ import express from 'express'
 
 const router = express.Router()
 
+/** Step-3: Setup Routes to serve dynamic sites with ejs */
+router.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Home Page'
+  })
+})
+
+router.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Page'
+  })
+})
+
+router.get('/services', (req, res) => {
+  res.render('services', {
+    title: 'Services Page'
+  })
+})
+
 export default router
--- server.js
+++ server.js
@@ -1,31 +1,9 @@
 import express from 'express'
 const app = express()
-
-/** Step-1: Tell Express about which template engine we are using */
+import mainRouter from './routes/index.js'
 app.set('view engine', 'ejs')
-console.log(app.get('view engine'))
 
-/** Step-2: Tell Express which folder to look for serving files: by default it is views folder */
-console.log(app.get('views'))
-
-/** Step-3: Setup Routes to serve dynamic sites with ejs */
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Home Page'
-  })
-})
-
-app.get('/about', (req, res) => {
-  res.render('about', {
-    title: 'About Page'
-  })
-})
-
-app.get('/services', (req, res) => {
-  res.render('services', {
-    title: 'Services Page'
-  })
-})
+app.use(mainRouter)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
